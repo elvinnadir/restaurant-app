c23a5bbd0078be0d12d0633efb482eb19cbe5d3b
Elvin <elvinnadir92@gmail.com>
2022-12-12 18:55:02 +0400

    restaurant-app

---
+++ src/container/Intro/Intro.jsx
@@ -0,0 +1,43 @@
+import React, { useRef, useState } from 'react';
+import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs';
+import { meal } from '../../constants';
+import './Intro.css';
+
+const Intro = () => {
+  const [playVideo, setPlayVideo] = useState(false);
+  const videoRef = useRef();
+
+  return (
+    <div className="app__video">
+      <video
+        ref={videoRef}
+        src={meal}
+        type="video/mp4"
+        loop
+        controls={false}
+        muted
+      />
+      <div className="app__video-overlay flex__center">
+        <div
+          className="app__video-overlay_circle flex__center"
+          onClick={() => {
+            setPlayVideo(!playVideo);
+            if (playVideo) {
+              videoRef.current.pause();
+            } else {
+              videoRef.current.play();
+            }
+          }}
+        >
+          {playVideo ? (
+            <BsPauseFill color="#fff" fontSize={30} />
+          ) : (
+            <BsFillPlayFill color="#fff" fontSize={30} />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Intro;
